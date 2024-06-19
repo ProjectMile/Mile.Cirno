dd01256d74b28ee0ec616841e8bbadc1b926a3fe
MouriNaruto <Kenji.Mouri@outlook.com>
2024-06-20 00:18:56 +0800

    Create Mile.Cirno.Protocol in Mile.Cirno.Core.

---
+++ Mile.Cirno.Core/Mile.Cirno.Protocol.h
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Cirno.Protocol.h
+ * PURPOSE:   Definition for Plan 9 File System Protocol
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef MILE_CIRNO_PROTOCOL
+#define MILE_CIRNO_PROTOCOL
+
+#include <stdint.h>
+
+#endif // !MILE_CIRNO_PROTOCOL
