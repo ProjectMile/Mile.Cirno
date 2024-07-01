3231f365dd89506db6af7ff273cab7cf737b8179
MouriNaruto <Kenji.Mouri@outlook.com>
2024-07-01 23:31:12 +0800

    Add Mile.Cirno project.

---
+++ Mile.Cirno/Mile.Cirno.cpp
@@ -0,0 +1,30 @@
+﻿/*
+ * PROJECT:   Mile.Cirno
+ * FILE:      Mile.Cirno.cpp
+ * PURPOSE:   Implementation for Mile.Cirno
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include <cstdint>
+
+#include <clocale>
+#include <cstdio>
+#include <cwchar>
+
+int main()
+{
+    // I can't use that because of the limitation in VC-LTL.
+    // std::setlocale(LC_ALL, "zh_CN.UTF-8");
+
+    std::setlocale(LC_ALL, "chs");
+
+    std::wprintf(
+        L"Mile.Cirno\n"
+        L"================================================================\n"
+        L"Hello World!\n");
+
+    return 0;
+}
