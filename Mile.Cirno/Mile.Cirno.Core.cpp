54a804e874593078ee0a9e57745b0be979dd921f
MouriNaruto <Kenji.Mouri@outlook.com>
2024-10-28 00:36:27 +0800

    Create the new Mile.Cirno.Core.

---
+++ Mile.Cirno/Mile.Cirno.Core.cpp
@@ -0,0 +1,14 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Cirno.Core.cpp
+ * PURPOSE:   Implementation for Mile.Cirno Core Infrastructures
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ *             per1cycle (pericycle.cc@gmail.com)
+ */
+
+#include "Mile.Cirno.Core.h"
+
+#include "Mile.Cirno.Protocol.Parser.h"
---
+++ Mile.Cirno/Mile.Cirno.Core.h
@@ -0,0 +1,21 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Cirno.Core.h
+ * PURPOSE:   Definition for Mile.Cirno Core Infrastructures
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ *             per1cycle (pericycle.cc@gmail.com)
+ */
+
+#ifndef MILE_CIRNO_CORE
+
+#include "Mile.Cirno.Protocol.h"
+
+namespace Mile::Cirno
+{
+
+}
+
+#endif // !MILE_CIRNO_CORE
