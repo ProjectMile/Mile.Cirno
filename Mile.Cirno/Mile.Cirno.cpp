ce1c74162d92f91ef1d4b0606ca3903712f8671b
MouriNaruto <Kenji.Mouri@outlook.com>
2025-08-15 00:00:41 +0800

    Formally implement ToNtStatus function in Mile.Cirno.

--- Mile.Cirno/Mile.Cirno.cpp
+++ Mile.Cirno/Mile.Cirno.cpp
@@ -34,6 +34,82 @@
 #include "Mile.Cirno.Core.h"
 #include "Mile.Cirno.Protocol.Parser.h"
 
+#include "Aptx.Linux.Error.h"
+
+NTSTATUS ToNtStatus(
+    std::uint32_t ErrorCode)
+{
+    switch (ErrorCode)
+    {
+    case 0:
+        return STATUS_SUCCESS;
+    case LX_EPERM:
+        return STATUS_NOT_SUPPORTED;
+    case LX_ENOENT:
+        return STATUS_OBJECT_NAME_NOT_FOUND;
+    case LX_ESRCH:
+        return STATUS_OBJECTID_NOT_FOUND;
+    case LX_EINTR: // fallthrough
+    case LX_ERESTARTSYS: // fallthrough
+    case LX_ERESTARTNOINTR: // fallthrough
+    case LX_ERESTARTNOHAND: // fallthrough
+    case LX_ERESTART_RESTARTBLOCK:
+        return STATUS_CANCELLED;
+    case LX_EIO:
+        return STATUS_IO_DEVICE_ERROR;
+    case LX_ENXIO:
+        return STATUS_NO_SUCH_DEVICE;
+    case LX_EBADF:
+        return STATUS_INVALID_HANDLE;
+    case LX_EAGAIN:
+        return STATUS_RETRY;
+    case LX_ENOMEM: // fallthrough
+    case LX_ENFILE: // fallthrough
+    case LX_EMFILE:
+        return STATUS_INSUFFICIENT_RESOURCES;
+    case LX_EACCES:
+        return STATUS_ACCESS_DENIED;
+    case LX_EFAULT:
+        return STATUS_ACCESS_VIOLATION;
+    case LX_EEXIST:
+        return STATUS_OBJECT_NAME_COLLISION;
+    case LX_ENODEV:
+        return STATUS_DEVICE_NOT_CONNECTED;
+    case LX_ENOTDIR:
+        return STATUS_NOT_A_DIRECTORY;
+    case LX_EISDIR:
+        return STATUS_FILE_IS_A_DIRECTORY;
+    case LX_EINVAL:
+        return STATUS_INVALID_PARAMETER;
+    case LX_EFBIG: // fallthrough
+    case LX_ENOSPC:
+        return STATUS_DISK_FULL;
+    case LX_EROFS:
+        return STATUS_MEDIA_WRITE_PROTECTED;
+    case LX_EMLINK: // fallthrough
+    case LX_ELOOP:
+        return STATUS_REPARSE_POINT_NOT_RESOLVED;
+    case LX_EPIPE:
+        return STATUS_CONNECTION_DISCONNECTED;
+    case LX_ERANGE:
+        return STATUS_INTEGER_OVERFLOW;
+    case LX_ENAMETOOLONG:
+        return STATUS_OBJECT_NAME_INVALID;
+    case LX_ENOTEMPTY:
+        return STATUS_DIRECTORY_NOT_EMPTY;
+    case LX_EOVERFLOW:
+        return STATUS_BUFFER_TOO_SMALL;
+    case LX_ENOMEDIUM:
+        return STATUS_NO_MEDIA_IN_DEVICE;
+    case LX_EMEDIUMTYPE:
+        return STATUS_UNRECOGNIZED_MEDIA;
+    default:
+        break;
+    }
+
+    return STATUS_UNSUCCESSFUL;
+}
+
 // Win32 time epoch is 00:00:00, January 1 1601.
 // UNIX time epoch is 00:00:00, January 1 1970.
 // There are 11644473600 seconds between these two epochs.
@@ -721,353 +797,4 @@ int main()
     Operations.SetFileSecurityW;
     Operations.FindStreams;
     return ::DokanMain(&Options, &Operations);
-}
-
-// References:
-// - https://github.com/freebsd/freebsd-src/tree/release/14.3.0
-//   - /sys/compat/linux/linux_errno.h
-//   - /sys/compat/linuxkpi/common/include/linux/errno.h
-// - https://git.musl-libc.org/cgit/musl/tree
-//   - /src/errno/__strerror.h
-
-// Operation not permitted
-#define LX_EPERM 1
-// No such file or directory
-#define LX_ENOENT 2
-// No such process
-#define LX_ESRCH 3
-// Interrupted system call
-#define LX_EINTR 4
-// I/O error
-#define LX_EIO 5
-// No such device or address
-#define LX_ENXIO 6
-// Argument list too long
-#define LX_E2BIG 7
-// Exec format error
-#define LX_ENOEXEC 8
-// Bad file descriptor
-#define LX_EBADF 9
-// No child process
-#define LX_ECHILD 10
-// Resource temporarily unavailable
-#define LX_EAGAIN 11
-// Out of memory
-#define LX_ENOMEM 12
-// Permission denied
-#define LX_EACCES 13
-// Bad address
-#define LX_EFAULT 14
-// Block device required
-#define LX_ENOTBLK 15
-// Resource busy
-#define LX_EBUSY 16
-// File exists
-#define LX_EEXIST 17
-// Cross-device link
-#define LX_EXDEV 18
-// No such device
-#define LX_ENODEV 19
-// Not a directory
-#define LX_ENOTDIR 20
-// Is a directory
-#define LX_EISDIR 21
-// Invalid argument
-#define LX_EINVAL 22
-// Too many open files in system
-#define LX_ENFILE 23
-// No file descriptors available
-#define LX_EMFILE 24
-// Not a tty
-#define LX_ENOTTY 25
-// Text file busy
-#define LX_ETXTBSY 26
-// File too large
-#define LX_EFBIG 27
-// No space left on device
-#define LX_ENOSPC 28
-// Invalid seek
-#define LX_ESPIPE 29
-// Read-only file system
-#define LX_EROFS 30
-// Too many links
-#define LX_EMLINK 31
-// Broken pipe
-#define LX_EPIPE 32
-// Domain error
-#define LX_EDOM 33
-// Result not representable
-#define LX_ERANGE 34
-// Resource deadlock would occur
-#define LX_EDEADLK 35
-// Filename too long
-#define LX_ENAMETOOLONG 36
-// No locks available
-#define LX_ENOLCK 37
-// Function not implemented
-#define LX_ENOSYS 38
-// Directory not empty
-#define LX_ENOTEMPTY 39
-// Symbolic link loop
-#define LX_ELOOP 40
-// Resource temporarily unavailable
-#define LX_EWOULDBLOCK LX_EAGAIN
-// No message of desired type
-#define LX_ENOMSG 42
-// Identifier removed
-#define LX_EIDRM 43
-#define LX_ECHRNG 44
-#define LX_EL2NSYNC 45
-#define LX_EL3HLT 46
-#define LX_EL3RST 47
-#define LX_ELNRNG 48
-#define LX_EUNATCH 49
-#define LX_ENOCSI 50
-#define LX_EL2HLT 51
-#define LX_EBADE 52
-#define LX_EBADR 53
-#define LX_EXFULL 54
-#define LX_ENOANO 55
-#define LX_EBADRQC 56
-#define LX_EBADSLT 57
-// Resource deadlock would occur
-#define LX_EDEADLOCK LX_EDEADLK
-#define LX_EBFONT 59
-// Device not a stream
-#define LX_ENOSTR 60
-// No data available
-#define LX_ENODATA 61
-// Device timeout
-#define LX_ENOTIME 62
-// Out of streams resources
-#define LX_ENOSR 63
-#define LX_ENONET 64
-#define LX_ENOPKG 65
-#define LX_EREMOTE 66
-// Link has been severed
-#define LX_ENOLINK 67
-#define LX_EADV 68
-#define LX_ESRMNT 69
-#define LX_ECOMM 70
-// Protocol error
-#define LX_EPROTO 71
-// Multihop attempted
-#define LX_EMULTIHOP 72
-#define LX_EDOTDOT 73
-// Bad message
-#define LX_EBADMSG 74
-// Value too large for data type
-#define LX_EOVERFLOW 75
-#define LX_ENOTUNIQ 76
-// File descriptor in bad state
-#define LX_EBADFD 77
-#define LX_EREMCHG 78
-#define LX_ELIBACC 79
-#define LX_ELIBBAD 80
-#define LX_ELIBSCN 81
-#define LX_ELIBMAX 82
-#define LX_ELIBEXEC 83
-// Illegal byte sequence
-#define LX_EILSEQ 84
-#define LX_ERESTART 85
-#define LX_ESTRPIPE 86
-#define LX_EUSERS 87
-// Not a socket
-#define LX_ENOTSOCK 88
-// Destination address required
-#define LX_EDESTADDRREQ 89
-// Message too large
-#define LX_EMSGSIZE 90
-// Protocol wrong type for socket
-#define LX_EPROTOTYPE 91
-// Protocol not available
-#define LX_ENOPROTOOPT 92
-// Protocol not supported
-#define LX_EPROTONOTSUPPORT 93
-// Socket type not supported
-#define LX_ESOCKNOTSUPPORT 94
-// Not supported
-#define LX_EOPNOTSUPPORT 95
-// Not supported
-#define LX_ENOTSUP LX_EOPNOTSUPPORT
-// Protocol family not supported
-#define LX_EPFNOTSUPPORT 96
-// Address family not supported by protocol
-#define LX_EAFNOTSUPPORT 97
-// Address in use
-#define LX_EADDRINUSE 98
-// Address not available
-#define LX_EADDRNOTAVAIL 99
-// Network is down
-#define LX_ENETDOWN 100
-// Network unreachable
-#define LX_ENETUNREACH 101
-// Connection reset by network
-#define LX_ENETRESET 102
-// Connection aborted
-#define LX_ECONNABORTED 103
-// Connection reset by peer
-#define LX_ECONNRESET 104
-// No buffer space available
-#define LX_ENOBUFS 105
-// Socket is connected
-#define LX_EISCONN 106
-// Socket not connected
-#define LX_ENOTCONN 107
-// Cannot send after socket shutdown
-#define LX_ESHUTDOWN 108
-#define LX_ETOOMANYREFS 109
-// Operation timed out
-#define LX_ETIMEDOUT 110
-// Connection refused
-#define LX_ECONNREFUSED 111
-// Host is down
-#define LX_EHOSTDOWN 112
-// Host is unreachable
-#define LX_EHOSTUNREACH 113
-// Operation already in progress
-#define LX_EALREADY 114
-// Operation in progress
-#define LX_EINPROGRESS 115
-// Stale file handle
-#define LX_ESTALE 116
-#define LX_EUCLEAN 117
-#define LX_ENOTNAM 118
-#define LX_ENAVAIL 119
-#define LX_EISNAM 120
-// Remote I/O error
-#define LX_EREMOTEIO 121
-// Quota exceeded
-#define LX_EDQUOT 122
-// No medium found
-#define LX_ENOMEDIUM 123
-// Wrong medium type
-#define LX_EMEDIUMTYPE 124
-// Operation canceled
-#define LX_ECANCELED 125
-// Required key not available
-#define LX_ENOKEY 126
-// Key has expired
-#define LX_EKEYEXPIRED 127
-// Key has been revoked
-#define LX_EKEYREVOKED 128
-// Key was rejected by service
-#define LX_EKEYREJECTED 129
-// Previous owner died
-#define LX_EOWNERDEAD 130
-// State not recoverable
-#define LX_ENOTRECOVERABLE 131
-#define LX_ERFKILL 132
-#define LX_EHWPOISON 133
-#define LX_ERESTARTSYS 512
-#define LX_ERESTARTNOINTR 513
-#define LX_ERESTARTNOHAND 514
-#define LX_ENOIOCTLCMD 515
-#define LX_ERESTART_RESTARTBLOCK 516
-#define LX_EPROBE_DEFER 517
-#define LX_EOPENSTALE 518
-#define LX_ENOPARAM 519
-
-BOOLEAN LxUtilLxErrorToNtStatus(INT ErrorCode, PNTSTATUS Status)
-{
-    BOOLEAN Result = TRUE;
-
-    switch (ErrorCode)
-    {
-    case 0:
-        *Status = STATUS_SUCCESS;
-        break;
-    case -LX_EPERM:
-        *Status = STATUS_NOT_SUPPORTED;
-        break;
-    case -LX_ENOENT:
-        *Status = STATUS_OBJECT_NAME_NOT_FOUND;
-        break;
-    case -LX_ESRCH:
-        *Status = STATUS_OBJECTID_NOT_FOUND;
-        break;
-    case -LX_EINTR: // fallthrough
-    case -LX_ERESTARTSYS: // fallthrough
-    case -LX_ERESTARTNOINTR: // fallthrough
-    case -LX_ERESTARTNOHAND: // fallthrough
-    case -LX_ERESTART_RESTARTBLOCK:
-        *Status = STATUS_CANCELLED;
-        break;
-    case -LX_EIO:
-        *Status = STATUS_IO_DEVICE_ERROR;
-        break;
-    case -LX_ENXIO:
-        *Status = STATUS_NO_SUCH_DEVICE;
-        break;
-    case -LX_EBADF:
-        *Status = STATUS_INVALID_HANDLE;
-        break;
-    case -LX_EAGAIN:
-        *Status = STATUS_RETRY;
-        break;
-    case -LX_ENOMEM: // fallthrough
-    case -LX_ENFILE: // fallthrough
-    case -LX_EMFILE:
-        *Status = STATUS_INSUFFICIENT_RESOURCES;
-        break;
-    case -LX_EACCES:
-        *Status = STATUS_ACCESS_DENIED;
-        break;
-    case -LX_EFAULT:
-        *Status = STATUS_ACCESS_VIOLATION;
-        break;
-    case -LX_EEXIST:
-        *Status = STATUS_OBJECT_NAME_COLLISION;
-        break;
-    case -LX_ENODEV:
-        *Status = STATUS_DEVICE_NOT_CONNECTED;
-        break;
-    case -LX_ENOTDIR:
-        *Status = STATUS_NOT_A_DIRECTORY;
-        break;
-    case -LX_EISDIR:
-        *Status = STATUS_FILE_IS_A_DIRECTORY;
-        break;
-    case -LX_EINVAL:
-        *Status = STATUS_INVALID_PARAMETER;
-        break;
-    case -LX_EFBIG: // fallthrough
-    case -LX_ENOSPC:
-        *Status = STATUS_DISK_FULL;
-        break;
-    case -LX_EROFS:
-        *Status = STATUS_MEDIA_WRITE_PROTECTED;
-        break;
-    case -LX_EMLINK: // fallthrough
-    case -LX_ELOOP:
-        *Status = STATUS_REPARSE_POINT_NOT_RESOLVED;
-        break;
-    case -LX_EPIPE:
-        *Status = STATUS_CONNECTION_DISCONNECTED;
-        break;
-    case -LX_ERANGE:
-        *Status = STATUS_INTEGER_OVERFLOW;
-        break;
-    case -LX_ENAMETOOLONG:
-        *Status = STATUS_OBJECT_NAME_INVALID;
-        break;
-    case -LX_ENOTEMPTY:
-        *Status = STATUS_DIRECTORY_NOT_EMPTY;
-        break;
-    case -LX_EOVERFLOW:
-        *Status = STATUS_BUFFER_TOO_SMALL;
-        break;
-    case -LX_ENOMEDIUM:
-        *Status = STATUS_NO_MEDIA_IN_DEVICE;
-        break;
-    case -LX_EMEDIUMTYPE:
-        *Status = STATUS_UNRECOGNIZED_MEDIA;
-        break;
-    default:
-        Result = FALSE;
-        *Status = STATUS_UNSUCCESSFUL;
-        break;
-    }
-
-    return Result;
 }
