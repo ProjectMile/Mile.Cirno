86a730f9525111a5faf375ed402b98d0f1e21b5a
MouriNaruto <Kenji.Mouri@outlook.com>
2025-08-15 21:25:39 +0800

    Introduce Aptx.Linux.Error.

---
+++ Mile.Cirno/Aptx.Posix.Error.h
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:    Application Portability Technology eXtensions
+ * FILE:       Aptx.Posix.Error.h
+ * PURPOSE:    Definition for POSIX Error Codes
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef APTX_POSIX_ERROR
+#define APTX_POSIX_ERROR
+
+
+
+#endif // !APTX_POSIX_ERROR
